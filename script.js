0044f16317e450552fa5b6bfbebef493c3ba2308
PiyushIngale007 <ingalepiyush@gmail.com>
2022-04-19 17:28:11 +0530

    assignment done

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+var principal = 0
+var years = 1
+var interest = 3.5
+
+function compute() {
+  if (principal && principal != 0) {
+    document.getElementById('result-box').style.display = 'block'
+
+    console.log(principal, years, interest);
+
+    var result = ((principal * years * interest) / 100).toFixed(2)
+    var year = new Date().getFullYear() + Math.floor(years)
+
+    document.getElementById('year').innerHTML = year
+    document.getElementById('interest').innerHTML = interest
+    document.getElementById('principal-amount').innerHTML = principal
+    document.getElementById('result').innerHTML = result
+
+  } else {
+    alert('Please Enter Principal Amount')
+  }
 }
-        
+
+function onInterestRateChange(value) {
+  document.getElementById('interest-rate').innerHTML = value + ' %'
+  interest = value
+}
+
+function onChangeYears(value) {
+  years = value
+}
+
+function onAmountChange(value) {
+    principal = value
+}
